5bb26047e14be08769eaf03bf5f46cf62130a91f
ftwpokedad <146678808+ftwpokedad@users.noreply.github.com>
2025-05-14 16:30:21 -0400

    Initial Commit

---
+++ TournamentDetails/TournamentDetailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TournamentDetailsApp.swift
+//  TournamentDetails
+//
+//  Created by Michael Woitalla on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct TournamentDetailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
